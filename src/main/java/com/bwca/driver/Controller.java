6148895bea3b41e3affa1a47f1b84e5b75cb11e7
Andres AG <andres.amayagarcia@bristol.ac.uk>
2018-12-10 12:30:43 +0000

    Fix null pointer exception in config file read

--- src/main/java/com/bwca/driver/Controller.java
+++ src/main/java/com/bwca/driver/Controller.java
@@ -193,7 +193,10 @@ public class Controller
                     break;
             }
         }
-        cfgConfig.loadFile(configFile);
+        if (configFile != null)
+        {
+            cfgConfig.loadFile(configFile);
+        }
 
         if (fail)
         {
