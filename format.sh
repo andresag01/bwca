d33c8f766cd522a01c68e26320e633a55578c378
Andres AG <andres.amayagarcia@bristol.ac.uk>
2019-02-28 14:35:00 +0000

    Fix format.sh clang format version

--- format.sh
+++ format.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env sh
 
+CLANG_FORMAT="clang-format-6.0"
+
 for src_file in $(find $1 -regex '.*\.java'); do
 	echo "    FMT   $src_file"
-	clang-format -i -style=file $src_file
+	$CLANG_FORMAT -i -style=file $src_file
 done
